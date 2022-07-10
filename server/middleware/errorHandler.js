0f9eac16abf36c03f423cd650ca0fc61977583c6
Rostislav Alekseev <mr.rostik.01@gmail.com>
2022-07-10 19:51:52 +0200

    modified error handler (again)

--- server/middleware/errorHandler.js
+++ server/middleware/errorHandler.js
@@ -1,13 +1,19 @@
-const chalk = require('chalk');
-const createHttpError = require('http-errors');
+const chalk = require("chalk");
+const createHttpError = require("http-errors");
 
 module.exports = {
   errorHandler(err, req, res, next) {
-    const status = err.status || 500;
-    const message = err.message || "Unexpected error occurred";
-
-    console.log(`Error:\nstatus - ${chalk.yellow(status)}\nmessage - ${chalk.red(message)}`);
-    res.status(status).json(message);
+    console.log(
+      `Error:\nstatus - ${chalk.yellow(err.status)}\nmessage - ${chalk.red(
+        err.message
+      )}`
+    );
+    if (createHttpError.isHttpError(err)) {
+      return res.status(err.status).json({ message: err.message });
+    }
+    return res
+      .status(500)
+      .json({ message: "Unexpected server error occurred" });
   },
   notFound(req, res, next) {
     next(createHttpError(404, "API route not found"));
